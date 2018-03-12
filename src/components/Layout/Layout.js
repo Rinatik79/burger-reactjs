7bdfe00db928b6712d8733d598750fa76ca365c2
Rinat Sabitov <sabitovrinat@gmail.com>
2018-03-11 23:25:38 -0400

    end 11 mart

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,3 +1,13 @@
-const layout = () => (
-    <div>Toolbar, SideDrawer, Backdrop </div>
-)
+import React from 'react';
+import Auxillary from '../../hoc/Auxillary'
+
+const layout = (props) => (
+    <Auxillary>
+        <div>Toolbar, SideDrawer, Backdrop </div>
+        <main>
+            {props.children}
+        </main>
+    </Auxillary>
+);
+
+export default layout;
