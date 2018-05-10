d24dda59d35443ab6a91b97f340934d93a1db8b4
Rinat Sabitov <sabitovrinat@gmail.com>
2018-05-09 22:26:28 -0400

    Order Now button works

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,8 +22,18 @@ class BurgerBuilder extends Component{
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
-    }  
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey];
+        }).reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+        this.setState({purchasable: sum > 0});
+    }
     
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -37,6 +47,7 @@ class BurgerBuilder extends Component{
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients:updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -54,6 +65,7 @@ class BurgerBuilder extends Component{
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients:updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
     
     render (){
@@ -66,7 +78,9 @@ class BurgerBuilder extends Component{
                 <Burger ingredients = {this.state.ingredients} />
                 <BuildControls ingredientAdded = {this.addIngredientHandler}
                 ingredientRemoved = {this.removeIngredientHandler}
-                disabled = {disabledInfo} />
+                disabled = {disabledInfo}
+                purchasable = {this.state.purchasable}
+                price = {this.state.totalPrice} />
             </Auxillary>
         );
     }
