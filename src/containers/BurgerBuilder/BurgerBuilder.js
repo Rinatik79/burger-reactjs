147c410ec61f86e0fa89fdcb629b4762e573ed0f
Rinat Sabitov <sabitovrinat@gmail.com>
2018-05-28 08:40:52 -0400

    First back-end implamentation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,9 @@ import Auxillary from '../../hoc/Auxillary/Auxillary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+//import { error } from 'util';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -80,7 +82,24 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        alert ("You continue!");
+        //alert ("You continue!");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Rinat',
+                adress: {
+                    street: 'Superstreet 114',
+                    town: 'Super town',
+                    country: 'Canada'
+                },
+                email: 'test@test.test'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+        .then(response => console.log(response));
+        //.catch(error => console.log(error));
     }
     
     render (){
